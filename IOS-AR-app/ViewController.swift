dd48314681967bf3a79ef6fa33380efe474eb90c
Jesse <davidjes@mail.gvsu.edu>
2019-11-24 14:49:18 -0500

    new

---
+++ IOS-AR-app/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  IOS-AR-app
+//
+//  Created by Xcode User on 11/24/19.
+//  Copyright © 2019 GVSU.CIS. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        let sun = createPlanet(radius: 0.25, image: "sun")
+        sun.position = SCNVector3(x:0,y:-0.5,z:-1)
+        
+        
+        // Create a new scene
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        sceneView.scene.rootNode.addChildNode(sun)
+    }
+    
+    func createPlanet(radius: Float, image: String) -> SCNNode{
+        let planet = SCNSphere(radius: CGFloat(radius))
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "\(image).jpg")
+        planet.materials = [material]
+    
+        let planetNode = SCNNode(geometry: planet)
+        
+        
+        return planetNode
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
---
+++ IOS-AR-appTests/IOS_AR_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  IOS_AR_appTests.swift
+//  IOS-AR-appTests
+//
+//  Created by Xcode User on 11/24/19.
+//  Copyright © 2019 GVSU.CIS. All rights reserved.
+//
+
+import XCTest
+@testable import IOS_AR_app
+
+class IOS_AR_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
